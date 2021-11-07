7ea0f7cb37b56784bf09db13f239eb0ae03cd243
haraldhultin <haraldhultin@gmail.com>
2021-11-07 19:51:19 +0100

    FileReadWrite 1-2

---
+++ FilesReadWrite/FilesReadWrite/MyFileHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesReadWrite
+{
+    class MyFileHandler
+    {
+        public void PrintAllContent(string path)
+        {
+            Console.WriteLine(File.ReadAllText(path) + " --" + path);
+        }
+        public void PrintAllContent(string[] stringArray)
+        {
+            foreach (var element in stringArray)
+            {
+                Console.WriteLine(element);
+            }
+        }
+        public string ReadAllContent(string path)
+        {
+            return File.ReadAllText(path);
+        }
+        public void WriteContentToFile(string text)
+        {
+            File.WriteAllText("testfile2.txt", text);
+        }
+
+        public void UpperCaseContent(string pathFileToReadFrom, string pathFileToWriteTo) 
+        {               
+            File.WriteAllText(pathFileToWriteTo, File.ReadAllText(pathFileToReadFrom).ToUpper());
+        }
+        public void ReadLatinEncodedContent(string pathFileToReadFrom)
+        {
+            Console.WriteLine(   File.ReadAllText(pathFileToReadFrom, Encoding.Latin1));
+        }
+        
+    }
+
+}
--- FilesReadWrite/FilesReadWrite/Program.cs
+++ FilesReadWrite/FilesReadWrite/Program.cs
@@ -8,6 +8,25 @@ namespace FilesReadWrite
     {
         static void Main(string[] args)
         {
+            MyFileHandler fileHandler = new();
+            //Console.WriteLine(fileHandler.ReadAllContent("testfile1.txt"));
+            //fileHandler.WriteContentToFile("hello town");
+            //fileHandler.PrintAllContent("testfile2.txt");
+            //fileHandler.UpperCaseContent("testfile1.txt", "testfile3.txt");
+            //fileHandler.PrintAllContent("testfile3.txt");
+            //fileHandler.ReadLatinEncodedContent("latin1.txt");
+
+            NoteBook notebook = new();
+            //notebook.TakeNote("first note");
+            //notebook.TakeNote("second note");
+            //notebook.TakeNote("buy milk");
+            //fileHandler.PrintAllContent("notes.txt");
+            //fileHandler.PrintAllContent(notebook.GetAllNotes());
+
+            notebook.AddNoteInputFromUser();
+            fileHandler.PrintAllContent("notes.txt");
+
+
         }
     }
 }
