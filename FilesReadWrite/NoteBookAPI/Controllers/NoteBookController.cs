6eba39940f72a77965d982c9d084db7da95fefbd
haraldhultin <haraldhultin@gmail.com>
2021-11-07 20:59:19 +0100

    crude get/post api for notbook

---
+++ FilesReadWrite/NoteBookAPI/Controllers/NoteBookController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilesReadWrite.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class NoteBookController : ControllerBase
+    {
+        NoteBook jeffTheNoteBoy = new NoteBook();
+        [HttpPost]
+        public void TakeNote(string note)
+        {
+            jeffTheNoteBoy.TakeNote(note);
+        }
+        [HttpGet]
+        public string[] GetAllNotes()
+        {
+            return jeffTheNoteBoy.GetAllNotes();
+        }
+    }
+}
---
+++ FilesReadWrite/NoteBookAPI/NoteBook.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesReadWrite
+{
+    class NoteBook
+    {
+        private readonly string _pathNotesFile = "notes.txt";
+        private readonly string _date = DateTime.Now.ToString();
+        // 59
+        public void TakeNote(string note)
+        {
+            if (!File.Exists(_pathNotesFile))
+            {
+                string dashedLine = new string('-', 39);
+                string _header = $"{"Datum",-24} Note \n{dashedLine}\n";
+                File.WriteAllText(_pathNotesFile, _header);
+            }
+            File.AppendAllText(_pathNotesFile, $"{_date,-24} {note} \n");
+        }
+        public string[] GetAllNotes() // Get all notes but ot not header.
+        {
+            if (!File.Exists(_pathNotesFile))
+            {
+                Console.WriteLine("file not found");
+            }
+            return File.ReadAllLines(_pathNotesFile)[2..];
+        }
+        public void AddNoteInputFromUser()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter text (enter EXIT to quit program) :");
+                string input = Console.ReadLine();
+                if (input == "EXIT") // leave loop if input is empty
+                    break;
+                TakeNote(input);
+            }
+        }
+
+
+    }
+}
