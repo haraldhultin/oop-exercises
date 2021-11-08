59face849392a3fc3485f7d278e98ab1c2560cb4
haraldhultin <haraldhultin@gmail.com>
2021-11-08 02:41:20 +0100

    fileStreams 1-1.4

--- FilesStreams/FilesStreams/MyFileHandler.cs
+++ FilesStreams/FilesStreams/MyFileHandler.cs
@@ -6,5 +6,67 @@ namespace FilesStreams
 {
     class MyFileHandler
     {
+        public void ReadAllContent(string path)
+        {
+            //using (FileStream fs = File.OpenRead(path))
+            //{
+            //    byte[] b = new byte[1024];
+            //    UTF8Encoding temp = new UTF8Encoding(true);
+            //    UnicodeEncoding unicode = new UnicodeEncoding();
+            //    while (fs.Read(b, 0, b.Length) > 0)
+            //    {
+            //        // Printing the file contents
+            //        Console.WriteLine(temp.GetString(b));
+            //    }
+            //}
+            using (FileStream fileStream = File.OpenRead(path))
+            {
+                StreamReader streamReader = new StreamReader(fileStream);
+                //string line = "";
+                //while (line != null)
+                //while (streamReader.Peek() != -1)
+                while (!streamReader.EndOfStream)
+                {
+                    //line = streamReader.ReadLine();
+                    //Console.WriteLine(line);
+                    Console.WriteLine(streamReader.ReadLine());
+                }
+
+            }
+        }
+            public void WriteContentToFile(string message, string path)
+            {
+                //FileStream fileStream = File.OpenWrite(path); // -- nödvändigt att gå via FileStream för att skapa StreamWriter?
+                using (StreamWriter streamWriter = new StreamWriter(path, append: true))
+                {                    
+                    streamWriter.WriteLine(message);
+                }
+            }
+        public void UpperCaseContent(string pathReadFrom, string pathWriteTo)
+        {
+            StreamReader streamReader = new StreamReader(pathReadFrom);
+            while (!streamReader.EndOfStream)
+            {
+                using (StreamWriter streamWriter = new StreamWriter(pathWriteTo, append: true))
+                {
+                    streamWriter.WriteLine(streamReader.ReadLine().ToUpper());
+                }
+            }
+            //FileStream fs = File.OpenRead(pathReadFrom); // CopyTo? hur? 
+            //FileStream fsWrite = File.OpenWrite(pathWriteTo);
+            //fs.CopyTo(fsWrite);
+            //fs.Close();
+            //fsWrite.Close();
+        }
+        public void ReadLatinEncodedContent()
+        {
+            using (FileStream fs = File.OpenRead("latin1.txt"))
+            {
+                for (int i = 0; i < fs.ToString().Length; i++)
+                {
+                    Console.WriteLine(fs.ReadByte());
+                }                 
+            }            
+        }
     }
 }
--- FilesStreams/FilesStreams/Program.cs
+++ FilesStreams/FilesStreams/Program.cs
@@ -7,6 +7,13 @@ namespace FilesStreams
         static void Main(string[] args)
         {
             MyFileHandler fileHandler = new MyFileHandler();
+            //fileHandler.ReadAllContent("testfile1.txt");
+            //fileHandler.WriteContentToFile("town", "testfile2.txt");
+            ////fileHandler.ReadAllContent("testfile2.txt");
+            fileHandler.UpperCaseContent("testfile1.txt", "testfile3.txt");
+            //fileHandler.ReadAllContent("testfile1.txt");
+            fileHandler.ReadAllContent("testfile3.txt");
+            //fileHandler.ReadLatinEncodedContent();
         }
     }
 }
